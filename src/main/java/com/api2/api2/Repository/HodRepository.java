e912adb488e576de6e765d28b62cfe8a59171fac
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-02-09 16:04:30 +0530

    Update HodRepository.java

--- src/main/java/com/api2/api2/Repository/HodRepository.java
+++ src/main/java/com/api2/api2/Repository/HodRepository.java
@@ -2,6 +2,7 @@ package com.api2.api2.Repository;
 
 import com.api2.api2.Models.BranchName;
 import com.api2.api2.Models.HOD;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
